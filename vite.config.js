aa250a84f9564d8c1101931de367970270aacd85
Neelam Makwana <hiyasoni5@gmail.com>
2024-02-06 13:36:13 +0530

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://neelammkw.github.io/MERN-basicpages-frontend"
+  base: "https://neelammkw.github.io/MERN_basic_frontend/"
 })

6f8b421cf2dd2e686df3908ae658eb4602496ef5
Neelam Makwana <hiyasoni5@gmail.com>
2024-02-07 10:22:59 +0530

    changes images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ export const Home = () => {
                 <div className="section-registration">
                     <div className="container grid grid-two-cols">
                         <div className="registration-img">
-                            <img src="images/3691028.jpg" alt="Login" width={500} height={400} />
+                            <img src="../images/3691028.jpg" alt="Login" width={500} height={400} />
                         </div>
                         {/* <div className="registration-form">
                             <h1 className="main-heading mb-3">
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+    '/api':"https://neelammkw.github.io/MERN_basic_frontend/",
+    },
+  },
   plugins: [react()],
   base: "https://neelammkw.github.io/MERN_basic_frontend/"
 })
